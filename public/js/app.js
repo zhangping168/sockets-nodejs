a2ec6ca5795d33bc5076a5d02e2cf1ffa3a701ab
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-26 01:24:44 -0400

    Add message to browser ui

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,9 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 	console.log("New message: ");
 	console.log(message.text);
+	
+	jQuery('.messages').append('<p>' + message.text + '</p>');
+	
 });
 
 var $form = jQuery('#message-form');
