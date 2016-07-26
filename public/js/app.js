856022ac0eab4ebfdbfce95c06eb9539a831cef9
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-26 01:11:33 -0400

    Added simple ui

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,22 @@
 var socket = io();
-socket.on('connect', function(){
+socket.on('connect', function () {
 	console.log('Connected to socket.io server');
 });
-socket.on('message', function(message){
+socket.on('message', function (message) {
 	console.log("New message: ");
 	console.log(message.text);
+});
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	$message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text : $form.find('input[name=message]').val()
+	});
+
+	$message.val('');
 });
