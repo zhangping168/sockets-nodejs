3c3757ae0a77dbc1653b90351d90582b2c2f1a29
zhangping168@gmail.com <zhangping168@gmail.com>
2016-07-26 00:45:08 -0400

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,7 @@ var socket = io();
 socket.on('connect', function(){
 	console.log('Connected to socket.io server');
 });
+socket.on('message', function(message){
+	console.log("New message: ");
+	console.log(message.text);
+});
